6c571958d4836acd453d3e682a86a0186d57e809
rookiecodein <kalumni20@gmail.com>
2023-07-14 11:05:05 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - alphabet game putchar
+ * 
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{       
+        int n = 97;
+        while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0)
+}
