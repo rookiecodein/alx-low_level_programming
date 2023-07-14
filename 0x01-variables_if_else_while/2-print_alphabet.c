8389021023b90e278a31ac94f0f7e4432343ee32
rookiecodein <kalumni20@gmail.com>
2023-07-14 16:29:12 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,13 @@
 
 int main(void)
 {
-	char i;
-	for (n = 'a' ; n <= 'z' ; n++)
-	{	
-		putchar(n);
+	char i = 'a';
+	while (i <= 'z')
+	{
+		if (i == 'e' ||  i == 'q')
+			i++;
+		putchar(i);
+		i++;
 	}
 	putchar('\n');
 	return (0);
