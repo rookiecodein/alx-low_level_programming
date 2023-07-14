441417cdd401496dd585bcd231cbe7d0f59164bf
rookiecodein <kalumni20@gmail.com>
2023-07-14 11:19:26 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
  * main - alphabet game putchar
- * 
+ *
  * Return: 0 (Success)
  */
 
 int main(void)
 {
-        char i;
-        for (n = 'a' ; n <= 'z' ; n++)
-                putchar(n);
-        putchar('\n');
-        return (0)
+	char i;
+	for (n = 'a' ; n <= 'z' ; n++)
+		putchar(n);
+	putchar('\n');
+	return (0)
 }
