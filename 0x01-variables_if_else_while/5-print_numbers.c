5dc79390c6b773c315c760f398dde223ee20e94d
rookiecodein <kalumni20@gmail.com>
2023-07-14 17:55:26 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - single digit
+ * Return: 0 success
+ */
+int main(void)
+{
+	int digit = 0
+	
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	printf("\n");
+	return (0);
+}
