1b50e7445cad8f9a8cf388ce7ff3d78944c7531d
rookiecodein <kalumni20@gmail.com>
2023-07-14 19:24:21 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - mirror reverse alphabet
+ * return: 0
+ */
+
+int main(void)
+{
+	char i = 'z';
+
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
