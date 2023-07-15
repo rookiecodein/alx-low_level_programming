bad0b52d1d285e31a337ca35d87f84d94ffbc01e
rookiecodein <kalumni20@gmail.com>
2023-07-14 19:58:54 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - combination singl digit
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	int dig = 0;
+
+	while (dig <= 9)
+	{
+		putchar(dig + 48);
+		
+		if (dig != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		dig++;
+	}
+	putchar('\n');
+	return (0);
+}
