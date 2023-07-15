52ef9e8c9fbdeaa5388eaca500827ba9a1e4785d
rookiecodein <kalumni20@gmail.com>
2023-07-14 18:10:45 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,12 +5,11 @@
  */
 int main(void)
 {
-	int digit = 0
+	int digit;
 
-	while (digit <= 9)
+	for (digit = 0; digit < 10; digit ++ )
 	{
 		printf("%d", digit);
-		digit++;
 	}
 	printf('\n');
 	return (0);
