eaa0ff9f0d29b535e77cd88dbfed0e4e6d17d853
rookiecodein <kalumni20@gmail.com>
2023-07-14 17:12:26 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,23 +9,19 @@ int main(void)
 	char i = 'a';
 	char I = 'A';
 		
-		/*prints a - z*/
-
-		while (i <= 'z')
-		{
-			putchar(i);
-				i++;
-		}
-
-                /*prints A - Z*/
-
-                while (I <= 'Z')
-                {
-                        putchar(I);
-                                I++;
-                }
-
-		putchar('\n');
+	/*prints a - z*/
+	while (i <= 'z')
+	{
+		putchar(i);
+			i++;
+	}
+        /*prints A - Z*/
+	 while (I <= 'Z')
+	{
+		putchar(I);
+			I++;
+	}
+	putchar('\n');
 
 		return (0);
 }
