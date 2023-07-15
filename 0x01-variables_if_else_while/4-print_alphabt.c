844c024f48f6a060321a33cc0902371c3d634f3f
rookiecodein <kalumni20@gmail.com>
2023-07-14 17:33:16 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - alphabet lowercase except
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char i= 'a';
+	while (i <= 'z')
+	{
+		if (i == 'e' || i == 'q')
+			i++;
+		putchar(i);
+		i++
+	}
+	putchar('\n)
+	return (0)
+}
