e141c17d0536df2a0d1d51ad88311cb3db79fbca
rookiecodein <kalumni20@gmail.com>
2023-07-18 12:41:37 -0700

    zz

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 /**
  * main - print putchar
  * Return: 0 success
  */
+
 int main(void)
 {
-	char str = "_putchar";
+	char str[] = "_putchar";
 	int i;
 
 	for (i = 0; i <= 8; i++)
