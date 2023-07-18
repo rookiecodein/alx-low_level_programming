21b824f605e404a1a8f0ef5580c53cf59f33b84c
rookiecodein <kalumni20@gmail.com>
2023-07-18 11:32:29 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,15 @@
  */
 int main(void)
 {
-	int i;
-	char c[] = "_putchar";
-
-	for (i = 0; i <= 10; i++)
-	{
-		putchar(c[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	
 	putchar('\n');
 	return (0);
 }
