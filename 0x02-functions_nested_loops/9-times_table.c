7756a3736719bd2fd68e3f9f227d43a119629322
rookiecodein <kalumni20@gmail.com>
2023-07-18 19:27:02 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * times_table - prints nine times table
+ */
+
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(48);
+		for (mult = 0; mult <= 9; mult++)
+		{
+			_putchar('.');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			/* put a space if product is a simple number
+			 * place the first digit if it is two numbers
+			 */
+			if (prod <= 9)
+				_putchar(' ');
+			else 
+				_putchar((prod / 10) + 48); /*get the first digit*/
+			_putchar((prod % 10) + 48); /*get the second digit*/
+		}
+		_putchar('\n');
+	}
+}
+
+
