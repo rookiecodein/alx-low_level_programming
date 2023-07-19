3a3de4ffa633199e1cd2f36174dbb79d8b9b97e5
rookiecodein <kalumni20@gmail.com>
2023-07-18 17:26:24 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - absolute value of integer
+ * @n: the input 
+ * Return: n absolute value
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	if else (n < 0)
+		return (n * -1);
+	}
+}
