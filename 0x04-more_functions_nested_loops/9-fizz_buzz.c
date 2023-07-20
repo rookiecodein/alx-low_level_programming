b50d9d928565e889e4051153ac04a7906cfbf69e
rookiecodein <kalumni20@gmail.com>
2023-07-20 14:03:25 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,7 +17,7 @@ int main(void)
 		else if (i % 5 == 0 && !(i % 3 == 0))
 			printf("Buzz");
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("Buzz");
+			printf("FizzBuzz");
 		else
 			printf("%d", i);
 
