336db580b398b7cfee4e5bb4a64a77457da234ce
rookiecodein <kalumni20@gmail.com>
2023-07-20 09:30:14 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draw a line in the webterm
+ * @n: number of time charc _ to be printed
+ * Return: 0 success
+ */
+
+void print_line(int n)
+{
+	int lnChr;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (lnChr = 1; lnChr <= n; lnChr++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
+
+
