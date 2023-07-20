755746a1dc4a49f8e08885db761194097c39a50b
rookiecodein <kalumni20@gmail.com>
2023-07-20 16:57:10 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triange of # character
+ * @size: size of triangle
+ * Return: 0 success
+ */
+
+void print_triangle(int size)
+{
+	int h, b;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (h = 1; h <= size; h++)
+		{
+			for (b = 1; b <= size; b++)
+			{
+				if ((h + b) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
+
