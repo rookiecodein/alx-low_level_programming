b0995f040e0a137f4f0a1b7503bd26d81fbfc375
rookiecodein <kalumni20@gmail.com>
2023-07-20 12:36:11 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_square - it prints square of n
+ * @size: is size of square
+ * Return: 0 success
+ */
+
+void print_square(int size)
+{
+	int row, column;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (column = 1; column <= size; column++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
+
