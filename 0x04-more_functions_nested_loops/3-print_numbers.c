20707a11cd2b27323048bd3a7ac0e2071d9a586a
rookiecodein <kalumni20@gmail.com>
2023-07-20 06:09:49 -0700

    void print_numbers(void)

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * Return: 0 success
+ */
+
+void print_numbers(void)
+{
+	int i = 0
+	
+	do
+	{
+		_putchar(i +48);
+		i++;
+	}
+	while (i >= 0 && i <=9);
+	_putchar('\n');
+}
+
