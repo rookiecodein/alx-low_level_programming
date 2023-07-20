d64ed9b534cd1af2d64a5230074cfa0433f70760
rookiecodein <kalumni20@gmail.com>
2023-07-20 11:01:35 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,21 +8,23 @@
 
 void print_diagonal(int n)
 {
-	int postn, space;
+	int tn, space;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (postn = 1; postn <= n; postn++)
+		for (tn = 1; tn <= n; tn++)
 		{
-			for (space = 1; space <= postn; space++)
+			for (space = 1; space <= tn; space++)
+			{
 				_putchar(' ');
 			_putchar(92);
 			/**
 			 * is equal to '/' char
 			 */
 			_putchar('\n');
+			}
 		}
 	}
 }
