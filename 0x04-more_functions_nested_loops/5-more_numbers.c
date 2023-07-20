5e1f2fdfda2c5c3b38e4a2af08471c5ae9bb87bb
rookiecodein <kalumni20@gmail.com>
2023-07-20 07:14:52 -0700

    void more_numbers(void)

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints ten times 0 to 14
+ * Return: 0 success
+ */
+
+void more_numbers(void)
+{
+	int i, lin, d;
+
+	for (lin = 0; lin < 10; lin++)
+	{
+		for (i >= 0; i < 14; i++)
+		{
+			i = d;
+			if (i > 9)
+			{	_putchar(1 + 48);
+				d = i % 10;
+			}
+			_putchar(d + 48):
+			}
+		_putchar('\n');
+	}
+}
+
