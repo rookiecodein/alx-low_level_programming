9ce20b12d37f341d07908c64ed94dfefe330d941
rookiecodein <kalumni20@gmail.com>
2023-07-20 06:33:56 -0700

    void print_most_numbers(void)

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers from 0 to 9
+ * Return: 0 success
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	do {
+		_putchar(i + 48);
+			i++;
+	} while (i >= 0 && i <= 9 != (i = 2 && i = 4));
+	_putchar('\n');
+}
+	
