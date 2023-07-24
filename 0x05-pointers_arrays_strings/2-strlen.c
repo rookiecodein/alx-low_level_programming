da2627838e7caaa1ace3b8f5ff4f82583764868e
rookiecodein <kalumni20@gmail.com>
2023-07-24 05:14:19 -0700

    int _strlen(char *s)

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - return lengh of string
+ * @s: integer
+ * Return: 0 success
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\0'; *s++)
+		counter++;
+	return (counter);
+}
+
+
