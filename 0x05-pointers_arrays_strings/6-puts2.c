7355900ceb941ec11620b966880aad8da1abff44
rookiecodein <kalumni20@gmail.com>
2023-07-24 07:23:46 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts2 - prints other character of string
+ * @str: input parameter
+ * Return: 0 success
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		i++;
+	}
+	t = i - 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2)
+		{
+		putchar(str[0]);
+		}
+	}
+	putchar('\n');
+	
+}
+
