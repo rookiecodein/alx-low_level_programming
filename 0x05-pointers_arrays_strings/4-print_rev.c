a4ca4d16a0ff8b7d5100bd439383443c3dc68fe8
rookiecodein <kalumni20@gmail.com>
2023-07-24 06:02:25 -0700

    void print_rev(char *s)

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: string parameter
+ * Return: 0 success
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i]);
+		i++;
+
+	while i--;
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
