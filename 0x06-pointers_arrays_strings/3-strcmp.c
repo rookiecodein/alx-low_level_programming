c9d159987f57247b776897fe8c487a25ce81ae9f
rookiecodein <kalumni20@gmail.com>
2023-07-27 08:21:15 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _strcmp - compares string values
- * s1: input value
- * S2: input value
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
  *
  * Return: s1[i] - s2[i]
  */
